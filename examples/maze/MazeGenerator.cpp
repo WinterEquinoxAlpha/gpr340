58a81e7b05faa076a070a12c08be0ec70f0a0733
WinterEquinoxAlpha <michael.kashian@mymail.champlain.edu>
2022-10-27 14:16:43 -0400

    Started work on maze generator

--- examples/maze/MazeGenerator.cpp
+++ examples/maze/MazeGenerator.cpp
@@ -2,6 +2,21 @@
 #include "World.h"
 
 bool MazeGenerator::Step(World* world) {
-  return false;
+	int worldSize = world->GetSize()/2;
+	for (int y = -worldSize; y <= worldSize; ++y)
+	{
+		for (int x = -worldSize; x <= worldSize; ++x)
+		{
+			Point2D p = {x, y};
+			world->SetNorth(p, false);
+			world->SetWest(p, false);
+		}
+		world->SetEast({ worldSize, y }, false);
+	}
+	for (int x = -worldSize; x <= worldSize; ++x)
+	{
+		world->SetSouth({ x, worldSize }, false);
+	}
+	return false;
 }
 void MazeGenerator::Clear(World* world) {}
