26d2382fa0b9dac74698868b1ca4b2999df192c1
WinterEquinoxAlpha <michael.kashian@mymail.champlain.edu>
2023-01-26 22:07:13 -0500

    Work on erosion

--- examples/scenario/generators/TerrainGenerator.cpp
+++ examples/scenario/generators/TerrainGenerator.cpp
@@ -152,7 +152,7 @@ float ConvolveStep(std::vector<float>* heights,
 				   std::vector<float>* waterVolume,
 				   std::vector<float>* materialVolume,
 				   std::vector<float>* pMaterialVolume,
-				   std::vector<float[4]>* waterFlow)
+				   std::vector<std::vector<float>>* waterFlow)
 {
 	switch (convolutionType)
 	{
@@ -166,16 +166,20 @@ float ConvolveStep(std::vector<float>* heights,
 		int index = linearize(p.x, p.y, sideSize);								// The index of the current pixel in the image
 		std::vector<float> g = (*gradients)[index];								// The elevation gradient for each direction of that pixel (E S W N)
 		std::vector<float> wg = CalculateGradient(waterVolume, sideSize, p);	// The water gradient for each direction of that pixel (E S W N)
+		float newHeight = (*heights)[index];									// The new elevation the function will return
 
-		Vector2 waterVelocity;	// The overall velocity of the water
-		float rainfall = 1.0f;	// The amout of water that is added each step
-		float flowRate = 1.0f;	// The rate of flow of water from one pixel to another
+		Vector2 waterVelocity;			// The overall velocity of the water
+		float rainfall = 1.0f;			// The amout of water that is added each step
+		float flowRate = 1.0f;			// The rate of flow of water from one pixel to another
+		float pickThreshold = 1.0f;		// The speed at which water will start to pick up material
+		float dropThreshold = 1.0f;		// The speed at which water will start to drop off material
+		float solubility = 1.0f;		// Ease of the material to be picked up
+		float evaporation = 0.9f;		// Percent of water that evaporates
 
-		// Simulate rainfall
+		// Step 1: Simulate rainfall
 		(*waterVolume)[index] += rainfall;
-		float currentWater = (*waterVolume)[index];
 
-		// Calculate the total water flow
+		// Step 2: Calculate the total water flow
 		float totalFlow = 0;
 		for (int i = 0; i < 4; ++i)
 		{
@@ -185,21 +189,76 @@ float ConvolveStep(std::vector<float>* heights,
 			(*waterFlow)[index][i] = std::max(0.0f, (*waterFlow)[index][i]);
 			totalFlow += (*waterFlow)[index][i];
 		}
-		if (totalFlow > currentWater)
+		if (totalFlow > (*waterVolume)[index])
 		{
-			float prop = currentWater / totalFlow;
+			float prop = (*waterVolume)[index] / totalFlow;
 			for (int i = 0; i < 4; ++i)
 			{
 				(*waterFlow)[index][i] *= prop;
 			}
 		}
 		float hOut = (*waterFlow)[index][0] - (*waterFlow)[index][2];
-		float hIn = (*waterFlow)[linearize(p.x + 1, p.y, sideSize)][2] - (*waterFlow)[linearize(p.x - 1, p.y, sideSize)][0];
+		float hIn;
+		if (p.x > 0 && p.x < sideSize - 1)
+			hIn = (*waterFlow)[linearize(p.x + 1, p.y, sideSize)][2] - (*waterFlow)[linearize(p.x - 1, p.y, sideSize)][0];
+		else
+			hIn = (*waterFlow)[index][2] - (*waterFlow)[index][0];
+
 		float vOut = (*waterFlow)[index][1] - (*waterFlow)[index][3];
-		float vIn = (*waterFlow)[linearize(p.x, p.y + 1, sideSize)][3] - (*waterFlow)[linearize(p.x, p.y - 1, sideSize)][1];
+		float vIn;
+		if (p.y > 0 && p.y < sideSize - 1)
+			vIn = (*waterFlow)[linearize(p.x, p.y + 1, sideSize)][3] - (*waterFlow)[linearize(p.x, p.y - 1, sideSize)][1];
+		else
+			vIn = (*waterFlow)[index][3] - (*waterFlow)[index][1];
+		
 		waterVelocity = {hOut + hIn, vOut + vIn};
 
-		// 
+		// Step 3: Calculate movement of material
+		float waterSpeed = waterVelocity.getMagnitude();
+		if (waterSpeed > pickThreshold)
+		{
+			float pick = waterSpeed * solubility;
+			(*materialVolume)[index] += pick;
+			newHeight -= pick;
+		}
+		if (waterSpeed < dropThreshold)
+		{
+			float prop = (dropThreshold - waterSpeed) / dropThreshold;
+			float drop = prop * (*materialVolume)[index];
+			std::cout << (*materialVolume)[index] << std::endl;
+			std::cout << "P: " << prop << ", D: " << drop << std::endl;
+			(*materialVolume)[index] -= drop;
+			newHeight += drop;
+		}
+
+		// Save the material info
+		(*pMaterialVolume)[index] = (*materialVolume)[index];
+
+		// Step 4: Move the water
+		float directions[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+		for (int i = 0; i < 4; ++i)
+		{
+			float currentWaterFlow = (*waterFlow)[index][i];
+			float prop = currentWaterFlow / totalFlow;
+			float materialFlow = prop * (*pMaterialVolume)[index];
+
+			Vector2 dd = {p.x + directions[i][0], p.y + directions[i][1]};
+			if (dd.x < 0 || dd.x >= sideSize)
+				dd.x = p.x;
+			if (dd.y < 0 || dd.y >= sideSize)
+				dd.y = p.y;
+
+			(*waterVolume)[index] -= currentWaterFlow;
+			(*waterVolume)[linearize(dd.x, dd.y, sideSize)] += currentWaterFlow;
+
+			(*materialVolume)[index] -= materialFlow;
+			(*materialVolume)[linearize(dd.x, dd.y, sideSize)] += materialFlow;
+		}
+
+		// Step 5: Evaporate water
+		(*waterVolume)[index] *= (1 - evaporation);
+
+		return newHeight;
 	}
 	return (*heights)[linearize(p.x, p.y, sideSize)];
 }
@@ -211,10 +270,18 @@ std::vector<Color32> TerrainGenerator::Convolve(int sideSize, float displacement
 		for (int i = 0; i < convolveSteps; ++i)
 		{
 			// For hydraulic erosion
-			std::vector<float> waterVolume;							// The amount of water on the current pixel
-			std::vector<float> materialVolume;						// The amount of material in the water
-			std::vector<float> pMaterialVolume = materialVolume;	// The previous amount of material
-			std::vector<float[4]> waterFlow;						// The amount of water that will flow in each direction
+			std::vector<float> waterVolume;				// The amount of water on the current pixel
+			std::vector<float> materialVolume;			// The amount of material in the water
+			std::vector<float> pMaterialVolume;			// The previous amount of material
+			std::vector<std::vector<float>> waterFlow;	// The amount of water that will flow in each direction
+
+			for (int i = 0; i < heights.size(); ++i)
+			{
+				waterVolume.emplace_back(0.0f);
+				materialVolume.emplace_back(0.0f);
+				pMaterialVolume.emplace_back(0.0f);
+				waterFlow.push_back({ 0.0f, 0.0f, 0.0f, 0.0f });
+			}
 			
 			std::vector<float> newHeights;
 			std::vector<float> newHeightsPlateau;
@@ -242,7 +309,7 @@ std::vector<Color32> TerrainGenerator::Convolve(int sideSize, float displacement
 				}
 			}
 
-			for (int i = 0; i < sideSize * sideSize; ++i)
+			for (int i = 0; i < heights.size(); ++i)
 			{
 				heights[i] = newHeights[i];
 				heightsPlateau[i] = newHeightsPlateau[i];
